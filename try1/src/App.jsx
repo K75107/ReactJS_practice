aea5b512083ad4e8816907cf4e6c2f994d487a7c
K75107 <divinagracia.kent123@gmail.com>
2025-01-18 23:41:06 +0800

    practice forms

--- try1/src/App.jsx
+++ try1/src/App.jsx
@@ -2,15 +2,43 @@ import ConditionalComponent from "./components/ConditionalComponent";
 import Hello from "./components/Hello";
 import Fruit from "./components/Fruit";
 import Message from "./components/message";
+import { useState } from "react";
 
 function App() {
-  function handleClickButton() {
-    return console.log("Hi");
+  const [count, setCount] = useState(0);
+  const [textValue, setTextValue] = useState({ firstName: "", lastName: "" });
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
   }
 
   return (
     <div>
-      <button onClick={handleClickButton}>Click me to see a message</button>
+      <h1>Count value is {count}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <form>
+        <input
+          onChange={(e) =>
+            setTextValue({ ...textValue, firstName: e.target.value })
+          }
+          type="text"
+          placeholder="Firstname"
+          value={textValue.firstName}
+        />
+        <input
+          onChange={(e) =>
+            setTextValue({ ...textValue, lastName: e.target.value })
+          }
+          type="text"
+          placeholder="Lastname"
+          value={textValue.lastName}
+        />
+      </form>
     </div>
   );
 }
